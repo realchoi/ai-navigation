c840ff29db47fc20d78f26fbc6cfdec62a067b7a
caidesen <caidesen@jee-cn.com>
2025-02-28 13:54:21 +0800

    声明 .vue 文件类型

--- src/types/globa.d.ts
+++
@@ -1,9 +0,0 @@
-import { DefineComponent } from "vue"
-
-// 声明 .vue 文件
-declare module "*.vue" {
-    const component: DefineComponent<object, object, any>
-    export default component
-}
-
-export { }
